60f61db3db29267d9a579d8cccf852bb3abb0cc3
Castrillonn <donjuancastrillon@gmail.com>
2024-03-26 11:35:44 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <Header 
+    :carrito="carrito"
+		:guitarra="guitarra"
+		@incrementar-cantidad="incrementarCantidad"
+		@decrementar-cantidad="decrementarCantidad"
+		@agregar-carrito="agregarCarrito"
+    />
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+        <div class="row mt-5">
+            <Guitarra 
+							v-for="guitarra in guitarras"
+							:guitarra="guitarra"
+							@agregar-carrito="agregarCarrito"
+            />
+        </div>
+    </main>
+    <Footer />
+</template>
+
+<!---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- SCRIPT -->
+<script setup>
+
+import { ref,reactive, onMounted } from 'vue';
+import { db } from './data/guitarras';
+import Guitarra from './components/Guitarra.vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
+const guitarras = ref([])
+const carrito = ref([])
+const guitarra = ref({})
+
+onMounted(() => {
+    guitarras.value = db
+		guitarra.value = db[8]
+})
+
+const agregarCarrito = (guitarra) => {
+	const exiteCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+	if (exiteCarrito >= 0) {
+		carrito.value[exiteCarrito].cantidad++
+	} else {
+		guitarra.cantidad = 1;
+		carrito.value.push(guitarra);
+	}
+}
+
+const decrementarCantidad = (id) => {
+	const index = carrito.value.findIndex(producto => producto.id === id)
+	if(carrito.value[index].cantidad <= 1) return
+	carrito.value[index].cantidad--
+}
+
+const incrementarCantidad = (id) => {
+	const index = carrito.value.findIndex(producto => producto.id === id)
+	if(carrito.value[index].cantidad >= 10) return
+	carrito.value[index].cantidad++
+} 
+
+</script>
+
+
+<!---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- STYLES -->
+<style scoped>
+
+
+</style>
